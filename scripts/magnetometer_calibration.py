b5c5eea5af070b064e0c2af9f02250b43118a843
Shehabeldin Abdelgawad <shehabeldin.abdelgawad@de.bosch.com>
2016-03-04 14:32:09 +0100

    rename

--- scripts/magnetometer_calibration.py
+++ scripts/magnetometer_calibration.py
@@ -6,13 +6,13 @@ import numpy as np
 if __name__ == '__main__':
     file_name_of_data_default = "Xsens_data_onboard_office.npy"
     path_of_file_load_default = "/home/abs8rng/catkin_ws/src/my_packages/low_cost_navigation/evaluation_code/data/imu_recordings/"
-    file_name_of_output_default = "some"
+    imu_name_default = "some"
     opt_sting = "phi:o:"
-    tutorial_string = "magnetometer_calibration.py -i <input_file_name(.npy)> -o <output_file_name(.yaml)> -p <path_of_input_file>"
+    tutorial_string = "magnetometer_calibration.py -i <input_file_name(.npy)> -o <IMU_name(.yaml)> -p <path_of_input_file>"
 
     file_name_of_data = file_name_of_data_default
     path_of_file_load = path_of_file_load_default
-    file_name_of_output = file_name_of_output_default
+    imu_name = imu_name_default
     try:
         opts, args = getopt.getopt(sys.argv[1:], opt_sting)
 
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         elif opt == "-i":
             file_name_of_data = arg
         elif opt == "-o":
-            file_name_of_output = arg
+            imu_name = arg
         elif opt == "-p":
             path_of_file_load = arg
         else:
@@ -36,14 +36,14 @@ if __name__ == '__main__':
 
     print "input data file = " + file_name_of_data
     print "input path of data file = " + path_of_file_load
-    print "calibration output file = " + file_name_of_output
+    print "calibration output file = " + imu_name
 
     if os.path.isfile(path_of_file_load + file_name_of_data):
         dic_from_drive = np.load(path_of_file_load + file_name_of_data).item()
         imu_mag_calib_obj = imu_mag_calibration(dic_from_drive)
         imu_mag_calib_obj.calculate_calib_parameters_hard_iron()
         imu_mag_calib_obj.calculate_calib_parameters_x_sqr()
-        imu_mag_calib_obj.dump_to_file(file_name_of_output)
+        imu_mag_calib_obj.dump_to_file(imu_name)
     else:
         print "No the given file has not been found"
         print path_of_file_load + file_name_of_data
